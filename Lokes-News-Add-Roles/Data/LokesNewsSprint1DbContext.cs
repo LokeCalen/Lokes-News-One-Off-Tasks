c7cef076b64b9d3cbc0e2949ae4eaa795071f7d5
LokeCalen <lokecalen@gmail.com>
2025-10-23 17:19:41 +0200

    a

--- Lokes-News-Add-Roles/Data/LokesNewsSprint1DbContext.cs
+++ Lokes-News-Add-Roles/Data/LokesNewsSprint1DbContext.cs
@@ -4,13 +4,13 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Lokes_News_Add_Roles.Data;
 
-public partial class LokesNewsDbContext : DbContext
+public partial class LokesNewsSprint1DbContext : DbContext
 {
-    public LokesNewsDbContext()
+    public LokesNewsSprint1DbContext()
     {
     }
 
-    public LokesNewsDbContext(DbContextOptions<LokesNewsDbContext> options)
+    public LokesNewsSprint1DbContext(DbContextOptions<LokesNewsSprint1DbContext> options)
         : base(options)
     {
     }
@@ -39,7 +39,7 @@ public partial class LokesNewsDbContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=LokesNewsDB;Trusted_Connection=True;");
+        => optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=LokesNewsSprint1DB;Trusted_Connection=True;");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
